40be0a196a22aba1501e0f2baa6d5124e1aec509
ggeorg <georgopoulos.georgios@gmail.com>
2015-12-19 01:15:32 +0200

    Added logging callbacks.

--- src/com/veracloud/logging/Log.java
+++ src/com/veracloud/logging/Log.java
@@ -8,280 +8,430 @@ import java.util.logging.Logger;
  * API for sending log output to {@link java.util.logging.Logger}.
  */
 public final class Log {
-  private static final String SELF = Log.class.getName();
+	private static final String SELF = Log.class.getName();
 
-  private final Logger mLogger;
+	private final Logger mLogger;
 
-  // WARN: Log constructor should have only package access so
-  // that only LogFactory be able to create one.
-  Log(String name) {
-    mLogger = Logger.getLogger(name);
-  }
+	// WARN: Log constructor should have only package access so
+	// that only LogFactory be able to create one.
+	Log(String name) {
+		mLogger = Logger.getLogger(name);
+	}
 
-  // ---------------------------
-  // TRACE
-  // ---------------------------
+	// ---------------------------
+	// TRACE
+	// ---------------------------
 
-  /**
-   * Is this logger instance enabled for the FINEST level?
-   * 
-   * @return True if this Logger is enabled for level FINEST, false otherwise.
-   */
-  public boolean isTraceEnabled() {
-    return mLogger.isLoggable(Level.FINEST);
-  }
+	/**
+	 * Is this logger instance enabled for the FINEST level?
+	 * 
+	 * @return True if this Logger is enabled for level FINEST, false otherwise.
+	 */
+	public boolean isTraceEnabled() {
+		return mLogger.isLoggable(Level.FINEST);
+	}
 
-  /**
-   * Log a message at level FINEST according to the specified format and
-   * arguments.
-   * 
-   * @param format
-   *          the format string
-   * @param args
-   *          the arguments
-   */
-  public void t(String format, Object... args) {
-    if (mLogger.isLoggable(Level.FINEST)) {
-      log(SELF, Level.FINEST, String.format(format, args), null);
-    }
-  }
+	/**
+	 * Log a message at level FINEST according to the specified format and
+	 * arguments.
+	 * 
+	 * @param format
+	 *            the format string
+	 * @param args
+	 *            the arguments
+	 */
+	public void t(String format, Object... args) {
+		if (mLogger.isLoggable(Level.FINEST)) {
+			log(SELF, Level.FINEST, String.format(format, args), null);
+		}
+	}
 
-  /**
-   * Log an exception (throwable) at level FINEST with an accompanying message
-   * according to the specified format and arguments.
-   * 
-   * @param t
-   *          the exception (throwable) to log
-   * @param format
-   *          the format string
-   * @param args
-   *          the arguments
-   */
-  public void t(Throwable t, String format, Object... args) {
-    if (mLogger.isLoggable(Level.FINEST)) {
-      log(SELF, Level.FINEST, String.format(format, args), t);
-    }
-  }
+	/**
+	 * Log an exception (throwable) at level FINEST with an accompanying logging
+	 * callback.
+	 * 
+	 * @param cb
+	 *            the logging callback to use
+	 */
+	public void t(Callback cb) {
+		if (mLogger.isLoggable(Level.FINEST)) {
+			log(SELF, Level.FINEST, cb.getMessage(), null);
+		}
+	};
 
-  // ---------------------------
-  // DEBUG
-  // ---------------------------
+	/**
+	 * Log an exception (throwable) at level FINEST with an accompanying message
+	 * according to the specified format and arguments.
+	 * 
+	 * @param t
+	 *            the exception (throwable) to log
+	 * @param format
+	 *            the format string
+	 * @param args
+	 *            the arguments
+	 */
+	public void t(Throwable t, String format, Object... args) {
+		if (mLogger.isLoggable(Level.FINEST)) {
+			log(SELF, Level.FINEST, String.format(format, args), t);
+		}
+	}
 
-  /**
-   * Is this logger instance enabled for the FINE level?
-   * 
-   * @return True if this Logger is enabled for level FINE, false otherwise.
-   */
-  public boolean isDebugEnabled() {
-    return mLogger.isLoggable(Level.FINE);
-  }
+	/**
+	 * Log an exception (throwable) at level FINEST with an accompanying logging
+	 * callback.
+	 * 
+	 * @param t
+	 *            the exception (throwable) to log
+	 * @param cb
+	 *            the logging callback to use
+	 */
+	public void t(Throwable t, Callback cb) {
+		if (mLogger.isLoggable(Level.FINEST)) {
+			log(SELF, Level.FINEST, cb.getMessage(), t);
+		}
+	};
 
-  /**
-   * Log a message at level FINE according to the specified format and
-   * arguments.
-   * 
-   * @param format
-   *          the format string
-   * @param args
-   *          the arguments
-   */
-  public void d(String format, Object... args) {
-    if (mLogger.isLoggable(Level.FINE)) {
-      log(SELF, Level.FINE, String.format(format, args), null);
-    }
-  }
+	// ---------------------------
+	// DEBUG
+	// ---------------------------
 
-  /**
-   * Log an exception (throwable) at level FINE with an accompanying message
-   * according to the specified format and arguments.
-   * 
-   * @param t
-   *          the exception (throwable) to log
-   * @param format
-   *          the format string
-   * @param args
-   *          the arguments
-   */
-  public void d(Throwable t, String format, Object... args) {
-    if (mLogger.isLoggable(Level.FINE)) {
-      log(SELF, Level.FINE, String.format(format, args), t);
-    }
-  }
+	/**
+	 * Is this logger instance enabled for the FINE level?
+	 * 
+	 * @return True if this Logger is enabled for level FINE, false otherwise.
+	 */
+	public boolean isDebugEnabled() {
+		return mLogger.isLoggable(Level.FINE);
+	}
 
-  // ---------------------------
-  // INFO
-  // ---------------------------
+	/**
+	 * Log a message at level FINE according to the specified format and
+	 * arguments.
+	 * 
+	 * @param format
+	 *            the format string
+	 * @param args
+	 *            the arguments
+	 */
+	public void d(String format, Object... args) {
+		if (mLogger.isLoggable(Level.FINE)) {
+			log(SELF, Level.FINE, String.format(format, args), null);
+		}
+	}
 
-  /**
-   * Is this logger instance enabled for the INFO level?
-   * 
-   * @return True if this Logger is enabled for the INFO level, false otherwise.
-   */
-  public boolean isInfoEnabled() {
-    return mLogger.isLoggable(Level.INFO);
-  }
+	/**
+	 * Log an exception (throwable) at level FINE with an accompanying logging
+	 * callback.
+	 * 
+	 * @param cb
+	 *            the logging callback to use
+	 */
+	public void d(Callback cb) {
+		if (mLogger.isLoggable(Level.FINE)) {
+			log(SELF, Level.FINE, cb.getMessage(), null);
+		}
+	};
 
-  /**
-   * Log a message at level INFO according to the specified format and
-   * arguments.
-   * 
-   * @param format
-   *          the format string
-   * @param args
-   *          the arguments
-   */
-  public void i(String format, Object... args) {
-    if (mLogger.isLoggable(Level.INFO)) {
-      log(SELF, Level.INFO, String.format(format, args), null);
-    }
-  }
+	/**
+	 * Log an exception (throwable) at level FINE with an accompanying message
+	 * according to the specified format and arguments.
+	 * 
+	 * @param t
+	 *            the exception (throwable) to log
+	 * @param format
+	 *            the format string
+	 * @param args
+	 *            the arguments
+	 */
+	public void d(Throwable t, String format, Object... args) {
+		if (mLogger.isLoggable(Level.FINE)) {
+			log(SELF, Level.FINE, String.format(format, args), t);
+		}
+	}
 
-  /**
-   * Log an exception (throwable) at level INFO with an accompanying message
-   * according to the specified format and arguments.
-   * 
-   * @param t
-   *          the exception (throwable) to log
-   * @param format
-   *          the format string
-   * @param args
-   *          the arguments
-   */
-  public void i(Throwable t, String format, Object... args) {
-    if (mLogger.isLoggable(Level.INFO)) {
-      log(SELF, Level.INFO, String.format(format, args), t);
-    }
-  }
+	/**
+	 * Log an exception (throwable) at level FINE with an accompanying logging
+	 * callback.
+	 * 
+	 * @param t
+	 *            the exception (throwable) to log
+	 * @param cb
+	 *            the logging callback to use
+	 */
+	public void d(Throwable t, Callback cb) {
+		if (mLogger.isLoggable(Level.FINE)) {
+			log(SELF, Level.FINE, cb.getMessage(), t);
+		}
+	};
 
-  // ---------------------------
-  // WARNING
-  // ---------------------------
+	// ---------------------------
+	// INFO
+	// ---------------------------
 
-  /**
-   * Is this logger instance enabled for the WARNING level?
-   * 
-   * @return True if this Logger is enabled for the WARNING level, false
-   *         otherwise.
-   */
-  public boolean isWarnEnabled() {
-    return mLogger.isLoggable(Level.WARNING);
-  }
+	/**
+	 * Is this logger instance enabled for the INFO level?
+	 * 
+	 * @return True if this Logger is enabled for the INFO level, false
+	 *         otherwise.
+	 */
+	public boolean isInfoEnabled() {
+		return mLogger.isLoggable(Level.INFO);
+	}
 
-  /**
-   * Log a message at level WARNING according to the specified format and
-   * arguments.
-   * 
-   * @param format
-   *          the format string
-   * @param args
-   *          the arguments
-   */
-  public void w(String format, Object... args) {
-    if (mLogger.isLoggable(Level.WARNING)) {
-      log(SELF, Level.WARNING, String.format(format, args), null);
-    }
-  }
+	/**
+	 * Log a message at level INFO according to the specified format and
+	 * arguments.
+	 * 
+	 * @param format
+	 *            the format string
+	 * @param args
+	 *            the arguments
+	 */
+	public void i(String format, Object... args) {
+		if (mLogger.isLoggable(Level.INFO)) {
+			log(SELF, Level.INFO, String.format(format, args), null);
+		}
+	}
 
-  /**
-   * Log an exception (throwable) at level WARNING with an accompanying message
-   * according to the specified format and arguments.
-   * 
-   * @param t
-   *          the exception (throwable) to log
-   * @param format
-   *          the format string
-   * @param args
-   *          the arguments
-   */
-  public void w(Throwable t, String format, Object... args) {
-    if (mLogger.isLoggable(Level.WARNING)) {
-      log(SELF, Level.WARNING, String.format(format, args), t);
-    }
-  }
+	/**
+	 * Log an exception (throwable) at level INFO with an accompanying logging
+	 * callback.
+	 * 
+	 * @param t
+	 *            the exception (throwable) to log
+	 * @param cb
+	 *            the logging callback to use
+	 */
+	public void i(Callback cb) {
+		if (mLogger.isLoggable(Level.INFO)) {
+			log(SELF, Level.INFO, cb.getMessage(), null);
+		}
+	};
 
-  // ---------------------------
-  // ERROR
-  // ---------------------------
+	/**
+	 * Log an exception (throwable) at level INFO with an accompanying message
+	 * according to the specified format and arguments.
+	 * 
+	 * @param t
+	 *            the exception (throwable) to log
+	 * @param format
+	 *            the format string
+	 * @param args
+	 *            the arguments
+	 */
+	public void i(Throwable t, String format, Object... args) {
+		if (mLogger.isLoggable(Level.INFO)) {
+			log(SELF, Level.INFO, String.format(format, args), t);
+		}
+	}
 
-  /**
-   * Is this logger instance enabled for level SEVERE?
-   * 
-   * @return True if this Logger is enabled for level SEVERE, false otherwise.
-   */
-  public boolean isErrorEnabled() {
-    return mLogger.isLoggable(Level.SEVERE);
-  }
+	/**
+	 * Log an exception (throwable) at level INFO with an accompanying logging
+	 * callback.
+	 * 
+	 * @param t
+	 *            the exception (throwable) to log
+	 * @param cb
+	 *            the logging callback to use
+	 */
+	public void i(Throwable t, Callback cb) {
+		if (mLogger.isLoggable(Level.INFO)) {
+			log(SELF, Level.INFO, cb.getMessage(), t);
+		}
+	};
 
-  /**
-   * Log a message at level SEVERE according to the specified format and
-   * arguments.
-   * 
-   * @param format
-   *          the format string
-   * @param args
-   *          the arguments
-   */
-  public void e(String format, Object... args) {
-    if (mLogger.isLoggable(Level.SEVERE)) {
-      log(SELF, Level.SEVERE, String.format(format, args), null);
-    }
-  }
+	// ---------------------------
+	// WARNING
+	// ---------------------------
 
-  /**
-   * Log an exception (throwable) at level SEVERE with an accompanying message
-   * according to the specified format and arguments.
-   * 
-   * @param t
-   *          the exception (throwable) to log
-   * @param format
-   *          the format string
-   * @param args
-   *          the arguments
-   */
-  public void e(Throwable t, String format, Object... args) {
-    if (mLogger.isLoggable(Level.SEVERE)) {
-      log(SELF, Level.SEVERE, String.format(format, args), t);
-    }
-  }
+	/**
+	 * Is this logger instance enabled for the WARNING level?
+	 * 
+	 * @return True if this Logger is enabled for the WARNING level, false
+	 *         otherwise.
+	 */
+	public boolean isWarnEnabled() {
+		return mLogger.isLoggable(Level.WARNING);
+	}
 
-  // ---------------------------
-  // Implementation
-  // ---------------------------
+	/**
+	 * Log a message at level WARNING according to the specified format and
+	 * arguments.
+	 * 
+	 * @param format
+	 *            the format string
+	 * @param args
+	 *            the arguments
+	 */
+	public void w(String format, Object... args) {
+		if (mLogger.isLoggable(Level.WARNING)) {
+			log(SELF, Level.WARNING, String.format(format, args), null);
+		}
+	}
 
-  private void log(String callerFQCN, Level level, String msg, Throwable t) {
-    LogRecord record = new LogRecord(level, msg);
-    record.setThrown(t);
-    fillCallerData(callerFQCN, record);
-    mLogger.log(record);
-  }
+	/**
+	 * Log an exception (throwable) at level WARNING with an accompanying
+	 * logging callback.
+	 * 
+	 * @param cb
+	 *            the logging callback to use
+	 */
+	public void w(Callback cb) {
+		if (mLogger.isLoggable(Level.WARNING)) {
+			log(SELF, Level.WARNING, cb.getMessage(), null);
+		}
+	};
 
-  private void fillCallerData(String callerFQCN, LogRecord record) {
-    StackTraceElement[] steArray = new Throwable().getStackTrace();
+	/**
+	 * Log an exception (throwable) at level WARNING with an accompanying
+	 * message according to the specified format and arguments.
+	 * 
+	 * @param t
+	 *            the exception (throwable) to log
+	 * @param format
+	 *            the format string
+	 * @param args
+	 *            the arguments
+	 */
+	public void w(Throwable t, String format, Object... args) {
+		if (mLogger.isLoggable(Level.WARNING)) {
+			log(SELF, Level.WARNING, String.format(format, args), t);
+		}
+	}
 
-    int selfIndex = -1;
-    for (int i = 0; i < steArray.length; i++) {
-      final String className = steArray[i].getClassName();
-      if (className.equals(callerFQCN)) {
-        selfIndex = i;
-        break;
-      }
-    }
+	/**
+	 * Log an exception (throwable) at level WARNING with an accompanying
+	 * logging callback.
+	 * 
+	 * @param t
+	 *            the exception (throwable) to log
+	 * @param cb
+	 *            the logging callback to use
+	 */
+	public void w(Throwable t, Callback cb) {
+		if (mLogger.isLoggable(Level.WARNING)) {
+			log(SELF, Level.WARNING, cb.getMessage(), t);
+		}
+	};
 
-    int found = -1;
-    for (int i = selfIndex + 1; i < steArray.length; i++) {
-      final String className = steArray[i].getClassName();
-      if (!(className.equals(callerFQCN))) {
-        found = i;
-        break;
-      }
-    }
+	// ---------------------------
+	// ERROR
+	// ---------------------------
 
-    if (found != -1) {
-      StackTraceElement ste = steArray[found];
-      // setting the class name has the side effect of setting
-      // the needToInferCaller variable to false.
-      record.setSourceClassName(ste.getClassName());
-      record.setSourceMethodName(ste.getMethodName());
-    }
-  }
+	/**
+	 * Is this logger instance enabled for level SEVERE?
+	 * 
+	 * @return True if this Logger is enabled for level SEVERE, false otherwise.
+	 */
+	public boolean isErrorEnabled() {
+		return mLogger.isLoggable(Level.SEVERE);
+	}
+
+	/**
+	 * Log a message at level SEVERE according to the specified format and
+	 * arguments.
+	 * 
+	 * @param format
+	 *            the format string
+	 * @param args
+	 *            the arguments
+	 */
+	public void e(String format, Object... args) {
+		if (mLogger.isLoggable(Level.SEVERE)) {
+			log(SELF, Level.SEVERE, String.format(format, args), null);
+		}
+	}
+
+	/**
+	 * Log an exception (throwable) at level SEVERE with an accompanying logging
+	 * callback.
+	 * 
+	 * @param cb
+	 *            the logging callback to use
+	 */
+	public void e(Callback cb) {
+		if (mLogger.isLoggable(Level.SEVERE)) {
+			log(SELF, Level.SEVERE, cb.getMessage(), null);
+		}
+	};
+
+	/**
+	 * Log an exception (throwable) at level SEVERE with an accompanying message
+	 * according to the specified format and arguments.
+	 * 
+	 * @param t
+	 *            the exception (throwable) to log
+	 * @param format
+	 *            the format string
+	 * @param args
+	 *            the arguments
+	 */
+	public void e(Throwable t, String format, Object... args) {
+		if (mLogger.isLoggable(Level.SEVERE)) {
+			log(SELF, Level.SEVERE, String.format(format, args), t);
+		}
+	}
+
+	/**
+	 * Log an exception (throwable) at level SEVERE with an accompanying logging
+	 * callback.
+	 * 
+	 * @param t
+	 *            the exception (throwable) to log
+	 * @param cb
+	 *            the logging callback to use
+	 */
+	public void e(Throwable t, Callback cb) {
+		if (mLogger.isLoggable(Level.SEVERE)) {
+			log(SELF, Level.SEVERE, cb.getMessage(), t);
+		}
+	};
+
+	// ---------------------------------------------------------------------
+	// Implementation (thanks to SLF4J)
+	// ---------------------------------------------------------------------
+
+	private void log(String callerFQCN, Level level, String msg, Throwable t) {
+		LogRecord record = new LogRecord(level, msg);
+		record.setThrown(t);
+		fillCallerData(callerFQCN, record);
+		mLogger.log(record);
+	}
+
+	private void fillCallerData(String callerFQCN, LogRecord record) {
+		StackTraceElement[] steArray = new Throwable().getStackTrace();
+
+		int selfIndex = -1;
+		for (int i = 0; i < steArray.length; i++) {
+			final String className = steArray[i].getClassName();
+			if (className.equals(callerFQCN)) {
+				selfIndex = i;
+				break;
+			}
+		}
+
+		int found = -1;
+		for (int i = selfIndex + 1; i < steArray.length; i++) {
+			final String className = steArray[i].getClassName();
+			if (!(className.equals(callerFQCN))) {
+				found = i;
+				break;
+			}
+		}
+
+		if (found != -1) {
+			StackTraceElement ste = steArray[found];
+			// setting the class name has the side effect of setting
+			// the needToInferCaller variable to false.
+			record.setSourceClassName(ste.getClassName());
+			record.setSourceMethodName(ste.getMethodName());
+		}
+	}
+
+	// ---------------------------
+
+	/** Logging callback. */
+	public static interface Callback {
+		String getMessage();
+	}
 }
