336191276c4ba876630c060d02510bdbcd2a15f1
ggeorg <georgopoulos.georgios@gmail.com>
2015-12-29 00:40:47 +0200

    Added support for loading resource logging.properties

--- src/com/veracloud/logging/LogFactory.java
+++ src/com/veracloud/logging/LogFactory.java
@@ -1,24 +1,50 @@
 package com.veracloud.logging;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.LogManager;
 
 public final class LogFactory {
-  //key: name (String), value: a Log instance
-  private static final Map<String, Log> loggerMap = new ConcurrentHashMap<String, Log>();
-  
-  public static Log get(Class<?> cls) {
-    return get(cls.getName());
-  }
-  
-  public static Log get(String name) {
-    Log logger = loggerMap.get(name);
-    if (logger != null) {
-      return logger;
-    } else {
-      Log newInstance = new Log(name);
-      Log oldInstance = loggerMap.putIfAbsent(name, newInstance);
-      return oldInstance == null ? newInstance : oldInstance;
-    }
-  }
+
+	static {
+		final LogManager logManager = LogManager.getLogManager();
+		final InputStream is = LogFactory.class.getResourceAsStream("/logging.properties");
+		if (is != null) {
+  		try {
+  			logManager.readConfiguration(is);
+  		} catch (SecurityException e) {
+  			e.printStackTrace();
+  		} catch (IOException e) {
+  			e.printStackTrace();
+  		} finally {
+  				try {
+  					is.close();
+  				} catch (IOException e) {
+  				  e.printStackTrace();
+  				}
+  		}
+		} else {
+		  System.err.println("Can't load resource: /logging.properties");
+		}
+	}
+
+	// key: name (String), value: a Log instance
+	private static final Map<String, Log> loggerMap = new ConcurrentHashMap<String, Log>();
+
+	public static Log get(Class<?> cls) {
+		return get(cls.getName());
+	}
+
+	public static Log get(String name) {
+		Log logger = loggerMap.get(name);
+		if (logger != null) {
+			return logger;
+		} else {
+			Log newInstance = new Log(name);
+			Log oldInstance = loggerMap.putIfAbsent(name, newInstance);
+			return oldInstance == null ? newInstance : oldInstance;
+		}
+	}
 }
